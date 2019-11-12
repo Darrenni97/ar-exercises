e44d5d93a356b4062ebe6b7af210d6c7f1ac7c89
Darrenni97 <darrenni_@hotmail.com>
2019-11-11 17:17:23 -0800

    completed exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
@@ -5,7 +7,20 @@ require_relative './exercise_3'
 require_relative './exercise_4'
 require_relative './exercise_5'
 
-puts "Exercise 6"
-puts "----------"
+puts 'Exercise 6'
+puts '----------'
 
 # Your code goes here ...
+class Store < ActiveRecord::Base
+  has_many :employees
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: 'Khurram', last_name: 'Virani', hourly_rate: 60)
+@store1.employees.create(first_name: 'John', last_name: 'Smith', hourly_rate: 30)
+@store1.employees.create(first_name: 'Bob', last_name: 'Doe', hourly_rate: 20)
+
+@store2.employees.create(first_name: 'Jane', last_name: 'Doe', hourly_rate: 40)
